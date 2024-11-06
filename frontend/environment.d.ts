7cfeb349e90b61127d6c5e2bb7dfbd7afbb866fd
Simon Wacker <simon.wacker@ise.fraunhofer.de>
2024-11-06 15:21:53 +0100

    Use file polling when `NEXT_WEBPACK_USEPOLLING` is set to `true` in ./frontend/.env.local (needed for fast refresh to work on Windows)

--- frontend/environment.d.ts
+++ frontend/environment.d.ts
@@ -2,6 +2,7 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv {
       NEXT_PUBLIC_METABASE_URL: string;
+      NEXT_WEBPACK_USEPOLLING: string;
       NODE_ENV: "test" | "development" | "production";
     }
   }
--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,5 +1,8 @@
-module.exports = {
-  webpack(config, options) {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  webpack: (config, options) => {
     config.module.rules.push({
       test: /\.graphql$/,
       exclude: /node_modules/,
@@ -18,6 +21,17 @@ module.exports = {
       use: "yaml-loader",
     });
 
+    // Enable polling based on env variable being set
+    // Inspired by https://medium.com/mikkotikkanen/solving-next-js-fast-refresh-on-docker-windows-71dfdb3ee785
+    if (process.env.NEXT_WEBPACK_USEPOLLING == "true" || false) {
+      config.watchOptions = {
+        poll: 500,
+        aggregateTimeout: 300
+      }
+    }
+
     return config;
   },
-};
+}
+
+module.exports = nextConfig;
